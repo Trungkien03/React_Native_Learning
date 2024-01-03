c8f9cd0a466551949d12c65193e86f4984e19e2b
trungkien03 <trungkiennguyen0310@gmail.com>
2024-01-03 23:02:01 +0700

    create screen layout and button component

---
+++ GuestNumber/screens/StartGameScreen.tsx
@@ -0,0 +1,11 @@
+import { TextInput, View } from 'react-native';
+
+const StartGameScreen = () => {
+    return (
+        <View>
+            <TextInput placeholder="Input Number..." />
+        </View>
+    );
+};
+
+export default StartGameScreen;
